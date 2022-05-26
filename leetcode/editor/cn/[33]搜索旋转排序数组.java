e14387c6ace672058fcd58476aaf47c07f4a58ec
endeavor-Steven <836412910@qq.com>
2022-05-26 20:58:39 +0800

    33.在旋转过的数组中找数

---
+++ leetcode/editor/cn/[33]搜索旋转排序数组.java
@@ -0,0 +1,103 @@
+package leetcode.editor.cn;
+
+//整数数组 nums 按升序排列，数组中的值 互不相同 。 
+//
+// 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[
+//k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2
+//,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。 
+//
+// 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [4,5,6,7,0,1,2], target = 0
+//输出：4
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [4,5,6,7,0,1,2], target = 3
+//输出：-1 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1], target = 0
+//输出：-1
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 5000 
+// -10^4 <= nums[i] <= 10^4 
+// nums 中的每个值都 独一无二 
+// 题目数据保证 nums 在预先未知的某个下标上进行了旋转 
+// -10^4 <= target <= 10^4 
+// 
+//
+// 
+//
+// 进阶：你可以设计一个时间复杂度为 O(log n) 的解决方案吗？ 
+// Related Topics 数组 二分查找 👍 2102 👎 0
+
+public class SearchInRotatedSortedArray{
+    public static void main(String[] args) {
+        Solution solution = new SearchInRotatedSortedArray().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int search(int[] nums, int target) {
+        //时间复杂度为n，看看怎么搞成logn
+//        int ans = -1;
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == target)
+//                ans = i;
+//        }
+//        return ans;
+
+        /**
+         * 正经方法：二分查找
+         * 加速遍历速度只有二分查找，但是这个题目不完全有序
+         * 核心思路解析：将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。
+         * 此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。就这样循环.
+         */
+        int n = nums.length;
+        int ans = -1;
+        if (n == 0) return ans;
+        if (n == 1) return nums[0] == target ? 0 : ans;
+
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target)
+                return mid; //成功的出口，不成功就走到最后
+            //开始划分
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
