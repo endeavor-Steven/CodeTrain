c4347f100e01476eb3d9368b5405a4107d41a6b7
endeavor-Steven <836412910@qq.com>
2022-05-30 15:01:45 +0800

    62.不同路径

---
+++ leetcode/editor/cn/[64]最小路径和.java
@@ -0,0 +1,81 @@
+package leetcode.editor.cn;
+
+//给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。 
+//
+// 说明：每次只能向下或者向右移动一步。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+//输出：7
+//解释：因为路径 1→3→1→1→1 的总和最小。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：grid = [[1,2,3],[4,5,6]]
+//输出：12
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == grid.length 
+// n == grid[i].length 
+// 1 <= m, n <= 200 
+// 0 <= grid[i][j] <= 100 
+// 
+// Related Topics 数组 动态规划 矩阵 👍 1260 👎 0
+
+public class MinimumPathSum{
+    public static void main(String[] args) {
+        Solution solution = new MinimumPathSum().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minPathSum(int[][] grid) {
+        /**
+         * 方法1：动态规划
+         * 路径思路：第一行的话，只能往右走；第一列的只能往下走；其他的取左边或者上面的路径和最小的
+         * 状态定义：dp[i][j]，是走到ij的最小路劲和
+         * 状态方程：
+         *  dp[i][j] = Min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+         *  dp[0][j] = dp[0][j-1] + grid[0][j];
+         *  dp[i][0] = dp[i-1][0] + grid[i][0];
+         * 状态边界：dp[0][0] = grid[0][0];
+         * 状态输出：dp[m-1][n-1];
+         */
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];//dp[i][j]从i走到j需要多少
+        dp[0][0] = grid[0][0];
+
+        //状态转移的三种状态
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
