d7c734899168d9b0fde629b3cffff5c6f7b113f1
endeavor-Steven <836412910@qq.com>
2022-05-30 15:18:26 +0800

    70.爬楼梯

---
+++ leetcode/editor/cn/[70]爬楼梯.java
@@ -0,0 +1,79 @@
+package leetcode.editor.cn;
+
+//假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
+//
+// 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：n = 2
+//输出：2
+//解释：有两种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶
+//2. 2 阶 
+//
+// 示例 2： 
+//
+// 
+//输入：n = 3
+//输出：3
+//解释：有三种方法可以爬到楼顶。
+//1. 1 阶 + 1 阶 + 1 阶
+//2. 1 阶 + 2 阶
+//3. 2 阶 + 1 阶
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= n <= 45 
+// 
+// Related Topics 记忆化搜索 数学 动态规划 👍 2444 👎 0
+
+public class ClimbingStairs{
+    public static void main(String[] args) {
+        Solution solution = new ClimbingStairs().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int climbStairs(int n) {
+        /**
+         * 方法1：动态规划
+         * 状态定义：dp[i],到某层有多少种方法
+         * 状态方程：dp[i] = sum(dp[i-1], dp[i-2]);
+         * 状态边界：dp[1] = 1;
+         * 状态输出：dp[n]；
+         */
+//        int[] dp = new int[n+1];
+//        if (n == 0) return 0;
+//        if (n == 1) return 1;
+//        if (n == 2) return 2;
+//        dp[0] = 0;
+//        dp[1] = 1;
+//        dp[2] = 2;
+//        for (int i = 3; i <= n; i++) {
+//            dp[i] = dp[i-1] + dp[i-2];
+//        }
+//        return dp[n];
+
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; i++) {
+            p = q;
+            q = r;
+            r = p + q;
+        }
+        return r;
+
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
