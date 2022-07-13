58e8181b8599acb53c0ffb2057bba06a25da81d4
endeavor-Steven <836412910@qq.com>
2022-07-13 16:56:33 +0800

    15、三数之和

--- leetcode/editor/cn/[15]三数之和.java
+++ leetcode/editor/cn/[15]三数之和.java
@@ -1,5 +1,5 @@
 package leetcode.editor.cn;
-
+import java.util.*;
 //给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重
 //复的三元组。 
 //
@@ -50,43 +50,42 @@ public class ThreeSum{
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         /**
-         * 1\特判，对于数组长度 nn，如果数组为 null 或者数组长度小于 3，返回 []。
-         * 2\对数组进行排序。
-         * 3\遍历排序后数组：
-         *  若 nums[i]>0因为已经排序好，所以后面不可能有三个数加和等于 0，直接返回结果。
-         *  对于重复元素：跳过，避免出现重复解
-         *  令左指针 L=i+1，右指针 R=n-1，当 L<R时，执行循环：
-         *      当 nums[i]+nums[L]+nums[R]==0，执行循环，判断左界和右界是否和下一位置重复，去除重复解。并同时将 L,R 移到下一位置，寻找新的解
-         *      若和大于 0，说明 nums[R] 太大，R左移
-         *      若和小于 0，说明 nums[L] 太小，L右移
-
+         * 排序+双指针
+         *
+         * 观察参数，发现长度是可能有问题的  len < 3 , 返回空
+         *
+         * 对原数组进行排序。然后开始枚举
+         * 对于三个指针的位置都要进行重复过滤。遇到一个点i， 使用双指针l = i + 1, r = n - 1;
+         * 求和，用划分的思想来移动指针，sum > 0, --r;   sum < 0, ++l
+         * 指针在移动的同时记得向前进方向跳过重复值
+         * 当枚举到nums[i] > 0的时候就无法出现和为0的情况了，即使剪枝
          */
         List<List<Integer>> ans = new ArrayList<>();
-        if (nums.length < 3) return ans;
-        Arrays.sort(nums);//方便过滤重复值
-
-        for (int i = 0; i < nums.length; i++) {
+        int n = nums.length;
+        if (n < 3)
+            return ans;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
             if (nums[i] > 0)
-               return ans;
-            if (i > 0 && nums[i] == nums[i-1])
+                return ans;
+            if (i > 0 && nums[i] == nums[i - 1])
                 continue;
-            int left = i + 1, right = nums.length - 1;
+            int left = i + 1, right = n - 1;
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if (sum == 0) {
-                    List<Integer> tmp = new ArrayList<>();
-                    tmp.add(nums[i]);
-                    tmp.add(nums[left]);
-                    tmp.add(nums[right]);
-                    ans.add(tmp);
-                    while (left < right && nums[left + 1] == nums[left])  ++left;
-                    while (left < right && nums[right - 1] == nums[right])  --right;
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1])
+                        ++left;
+                    while (left < right && nums[right] == nums[right - 1])
+                        --right;
+                    //迭代
                     ++left;
                     --right;
-                } else if (sum > 0) {
-                    --right;
+                } else if (sum < 0) {
+                    ++left;
                 } else {
-                    ++left;
+                    --right;
                 }
             }
         }
