bb64fcb80cc2d8dec74e98c4fca09f3b9025c91e
endeavor-Steven <836412910@qq.com>
2022-07-13 22:28:05 +0800

    21.合并两个有序链表

--- leetcode/editor/cn/[21]合并两个有序链表.java
+++ leetcode/editor/cn/[21]合并两个有序链表.java
@@ -1,5 +1,5 @@
 package leetcode.editor.cn;
-
+import java.util.*;
 //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
 //
 // 
@@ -39,20 +39,10 @@ package leetcode.editor.cn;
 public class MergeTwoSortedLists{
     public static void main(String[] args) {
         Solution solution = new MergeTwoSortedLists().new Solution();
-        
     }
 
 //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 
 class Solution {
     public class ListNode {
@@ -63,23 +53,25 @@ class Solution {
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        /**
+         * 尾插法建表
+         */
         ListNode ans = new ListNode();
-        ListNode rear = ans;
+        ListNode rear = ans;//尾指针
         while (list1 != null && list2 != null) {
-            ListNode tmp = new ListNode();
             if (list1.val <= list2.val) {
-                tmp = list1;
+                rear.next = list1;
                 list1 = list1.next;
-                rear.next = tmp;
             } else {
-                tmp = list2;
+                rear.next = list2;
                 list2 = list2.next;
-                rear.next = tmp;
             }
             rear = rear.next;
         }
-        if (list1 != null) rear.next = list1;
-        if (list2 != null) rear.next = list2;
+        if (list1 != null)
+            rear.next = list1;
+        if (list2 != null)
+            rear.next = list2;
         return ans.next;
     }
 }
