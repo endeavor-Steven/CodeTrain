dc91367343bddf85b871b10e025b6ce334eb030e
endeavor-Steven <836412910@qq.com>
2022-07-26 13:23:10 +0800

    287.寻找重复值

--- leetcode/editor/cn/[287]寻找重复数.java
+++ leetcode/editor/cn/[287]寻找重复数.java
@@ -78,24 +78,6 @@ class Solution {
             slow = nums[slow];
         }
         return ans;
-
-
-
-
-//        //可以，但是超时了
-//        int ans = 0;
-//        for (int i = 0; i < nums.length; i++) {
-//            int count = 0;
-//            for (int j = 0; j < nums.length; j++) {
-//                if (nums[i] == nums[j] && i != j) {
-//                    ++count;
-//                }
-//                if (count == 1) {
-//                    ans = nums[i];
-//                }
-//            }
-//        }
-//        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
