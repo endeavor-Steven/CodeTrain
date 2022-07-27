eaf7d673970ff0b737e3a8bd0faa587ba66ee566
endeavor-Steven <836412910@qq.com>
2022-07-27 10:20:54 +0800

    337.打家劫舍Ⅲ

--- leetcode/editor/cn/[337]打家劫舍 III.java
+++ leetcode/editor/cn/[337]打家劫舍 III.java
@@ -81,9 +81,14 @@ class Solution {
          * 最优子结构 Max(爷爷节点偷的钱+四个孙子投的钱, 两个儿子投的钱)
          * 重复问题：爷爷节点偷钱的时候，其实也想到过四个孙子可以偷多少， 以及两个儿子可以偷多少
          *      解决方法，在数组结构中可以使用数组存储记忆，但是在树形结构里没办法，那么采用HashMap存储(Node, robMoney)
+         *      可以进一步优化这个重复子问题
          * 优化：
          *      每个节点的状态可以被定义为偷或者不偷，且相邻节点不能都偷
          *      换句话说，一个节点偷则孩子节点不能偷；一个节点不偷则孩子节点看情况偷只要钱最多就行
+         *
+         *      定义：dp[2], 0不偷，1偷
+         *      转移：当前节点不偷root[0] = max(root.left[1], root.left[0]) + max(root.right[0], root.right[1])
+         *           当前节点要偷root[1] = root.left[0] + root.right[0] + root.val
          */
         int[] ans = robed(root);
         return Math.max(ans[0], ans[1]);
@@ -91,8 +96,9 @@ class Solution {
     public int[] robed (TreeNode root) {
         int[] dp = new int[2];
         if (root == null)   return dp;
-        int left[] =robed(root.left);
-        int right[] =robed(root.right);
+        int[] left =robed(root.left);
+        int[] right =robed(root.right);
+        //后序遍历，从叶子方向往上偷
         dp[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
         dp[1] = root.val + left[0] + right[0];
         return dp;
