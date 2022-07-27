f34a34e5ad68c109943490bcd39d69fd0485c73a
endeavor-Steven <836412910@qq.com>
2022-07-27 10:21:10 +0800

    338.比特位计数

--- leetcode/editor/cn/[338]比特位计数.java
+++ leetcode/editor/cn/[338]比特位计数.java
@@ -61,22 +61,59 @@ public class CountingBits{
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] countBits(int n) {
-        //使用奇偶性特征
+        /**
+         * 使用奇偶性特征
+         * 对于所有的数字，只有两类：
+         * --奇数：二进制表示中，奇数一定比前面那个偶数多一个 1，因为多的就是最低位的 1。
+         *    举例： 0 = 0       1 = 1
+         *          2 = 10      3 = 11
+         * --偶数：二进制表示中，偶数中 1 的个数一定和除以 2 之后的那个数一样多。因为最低位是 0，除以 2 就是右移一位，也就是把那个 0 抹掉而已，所以 1 的个数是不变的。
+         *   举例：   2 = 10       4 = 100       8 = 1000
+         *           3 = 11       6 = 110       12 = 1100
+         * --另外，0 的 1 个数为 0，于是就可以根据奇偶性开始遍历计算了。
+         *
+         */
+//        int[] ans = new int[n + 1];
+//        ans[0] = 0;
+//        for (int i = 1; i <= n; i++) {
+//            if (i % 2 == 1)
+//                ans[i] = ans[i-1] + 1;
+//            else
+//                ans[i] = ans[i / 2];
+//        }
+//        return ans;
 
-        //可以，但是效率低
-        if (n == 0)
-            return new int[]{0};
-        int[] ans = new int[n + 1];
-        ans[0] = 0;
+        int[] result = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            int count = 0;
-            for (int j = i; j != 0 ; j = j >> 1) {
-                if ((j & 1) == 1)
-                    ++count;
-            }
-            ans[i] = count;
+            result[i] = result[i / 2] + i % 2; //可以改成位运算加速
         }
-        return ans;
+        return result;
+
+
+
+
+
+
+
+
+
+
+
+
+//        //可以，但是效率低
+//        if (n == 0)
+//            return new int[]{0};
+//        int[] ans = new int[n + 1];
+//        ans[0] = 0;
+//        for (int i = 1; i <= n; i++) {
+//            int count = 0;
+//            for (int j = i; j != 0 ; j = j >> 1) {
+//                if ((j & 1) == 1)
+//                    ++count;
+//            }
+//            ans[i] = count;
+//        }
+//        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
