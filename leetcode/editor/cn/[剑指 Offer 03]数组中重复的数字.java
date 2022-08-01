14f513831b9007a0f8260a7dade51188c9e2e0b1
endeavor-Steven <836412910@qq.com>
2022-08-01 09:44:15 +0800

    剑指Offer03.数组中重复的数字

--- leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.java
+++ leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.java
@@ -30,60 +30,34 @@ public class ShuZuZhongZhongFuDeShuZiLcof{
 class Solution {
     public int findRepeatNumber(int[] nums) {
         /**
-         * 思路分析：
-         * 原地交换
-         * 枚举过程中，遇到一个值x， 把他放到nums[x]中。当第二次遇到x的时候，会有nums[x] = x,此时得到重复值
-         * 可能遇到的情况
-         *  nums[i] = i, 已经对应了，不管
-         *  nums[nums[i]] = nums[i], 表示索引nums[i]处和索引i处的值都是nums[i],也就是一组重复值，返回nums[i];
-         *  不满足上述两种情况则交换i和nums[i]的元素值，把数字换到索引的索引位置
+         * 关键审题：
+         *      一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内
          *
-         *
-         *  （我的理解）
-         * 更优的方法：
-         * 原地交换（散列映射）————用数组模拟hash的结构出现地址冲突就说明有重复值
-         * 我的目的是在初次遍历过程中完成一种散列关系
-         * nums[i] = [i];
-         * 对每个散列关系进行归位的时候会存在覆盖，为了防止覆盖就交换一下被覆盖的值。依次散列。直到符合关系。
-         * 当nums[nums[i]] = nums[i]的时候就意味着已经覆盖过了。（二次跳转都符合关系的话）就是答案了
+         * 最优解法：原地交换
+         *      核心思路：将数组的num存储为index，此时数组变成字典数组（键值关系nums[num] = num）
+         *              可以通过搜索下标位置找到值
+         *      算法流程：
+         *              在枚举过程中，第一次遇到x，调整位置为index = x处，即numx[x] = x;
+         *              第二次遇到x时，肯定是已经有了nums[x]=x的，则可判定此时找到了一组重复数
          */
         int n = nums.length;
         int i = 0;
-        while (i < n) {
-            //本质的核心是希望构建一个nums[x] = x的映射关系，当二次检索到这种关系的时候就说明是重复值（一对多）。
-            if (nums[i] == i) {
+        while (i < n) { //本质的核心是希望构建一个nums[x] = x的映射关系，当二次检索到这种关系的时候就说明是重复值（一对多）。
+            if (nums[i] == i) {//此处自然符合排序要求，则准备寻找下一个
                 i++;
                 continue;
             }
-            if (nums[nums[i]] == nums[i])
+            if (nums[nums[i]] == nums[i])//此处index的值的归序位置已经有值了
+            //nums[i]是准备当前枚举到的值，准备将其从index = i 调整到index = nums[i]的位置，后者的值为nums[nums[i]]。交换重排
                 //找到一个值nums[i]他散列的结果还是他本身nums[nums[i]]说明已经排序过了
                 return nums[i];
-            //目的是把散列占位的值交换出来，避免覆盖
-            int tmp = nums[i];
+            //又没有天然归序，又不是重复值，则交换index = i 和 index = nums[i]的位置，完成一次归序
+            int tmp = nums[i];//注意此处的交换的Index写法，很容易出现覆盖的情况
             nums[i] = nums[tmp];
             nums[tmp] = tmp;
         }
         return -1;
     }
-
-
-//        /**
-//         * Set
-//         */
-//    public int findRepeatNumber(int[] nums) {
-//        int n = nums.length;
-//        if (n == 2)
-//            return nums[0];
-//        Set<Integer> set = new HashSet<>();
-//        for (int i = 0; i < n; i++) {
-//            if (set.contains(nums[i]))
-//                return nums[i];
-//            else
-//                set.add(nums[i]);
-//        }
-//        return 0;
-//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
-
 }
