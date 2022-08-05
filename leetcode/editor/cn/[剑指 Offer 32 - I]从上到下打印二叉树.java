160d2255a1e6c745d327d436ff593156d0477f74
endeavor-Steven <836412910@qq.com>
2022-08-05 14:19:36 +0800

    剑指Offer32.从上到下打印二叉树

---
+++ leetcode/editor/cn/[剑指 Offer 32 - I]从上到下打印二叉树.java
@@ -0,0 +1,80 @@
+package leetcode.editor.cn;
+
+//从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。 
+//
+// 
+//
+// 例如: 
+//给定二叉树: [3,9,20,null,null,15,7], 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+// 
+//
+// 返回： 
+//
+// [3,9,20,15,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 节点总数 <= 1000 
+// 
+// Related Topics 树 广度优先搜索 二叉树 👍 226 👎 0
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CongShangDaoXiaDaYinErChaShuLcof{
+    public static void main(String[] args) {
+        Solution solution = new CongShangDaoXiaDaYinErChaShuLcof().new Solution();
+        
+    }
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    class TreeNode {
+        int val;
+        TreeNode left, right;
+        TreeNode(int x){this.val = x;}
+    }
+    public int[] levelOrder(TreeNode root) {
+        if (root == null)   return new int[0];
+        Deque<TreeNode> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            TreeNode tmp = q.poll();
+            ans.add(tmp.val);
+            if (tmp.left != null)
+                q.offer(tmp.left);
+            if (tmp.right != null)
+                q.offer(tmp.right);
+        }
+        int[] res = new int[ans.size()];
+        for (int i = 0; i < ans.size(); i++) {
+            res[i] = ans.get(i);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
